441d185facd58ad36306a2905593597188a84a48
xtruan <github@xtruan.com>
2020-06-09 22:13:16 -0600

    stubbing out lighting code

--- raspi/action_recorder.py
+++ raspi/action_recorder.py
@@ -9,11 +9,17 @@ key_mode = 0
 
 def on_press(key):
     global csv_file, start, key_mode
+
+    if key == Key.esc:
+        # Stop listener
+        return False
+
     if key_mode <= 0:
         key_mode = 1
         
         end = time.time()
-        out_str = transform.time_stringify(end - start) + ',8,SERVO,2,90'
+        act = key_to_actuator(key)
+        out_str = transform.time_stringify(end - start) + ',8,' + act + ',90'
         print(out_str)
         csv_file.write(out_str + '\n')
 
@@ -23,18 +29,39 @@ def on_release(key):
         key_mode = -1
         
         end = time.time()
-        out_str = transform.time_stringify(end - start) + ',8,SERVO,2,00'
+        act = key_to_actuator(key)
+        out_str = transform.time_stringify(end - start) + ',8,' + act + ',00'
         print(out_str)
         csv_file.write(out_str + '\n')
 
-    if key == Key.esc:
-        # Stop listener
-        return False
     if key == Key.backspace:
         print('--RESET--')
         csv_file.close()
         csv_file = open('rec.csv', 'w')
         start = time.time()
+
+def key_to_actuator(key):
+    key_str = str(key).replace("'", "")
+    if key == Key.space:
+        return 'SERVO,1'
+    if key_str == 'a':
+        return 'LIGHT,1'
+    elif key_str == 's':
+        return 'LIGHT,2'
+    elif key_str == 'd':
+        return 'LIGHT,3'
+    elif key_str == 'f':
+        return 'LIGHT,4'
+    elif key_str == 'j':
+        return 'LIGHT,5'
+    elif key_str == 'k':
+        return 'LIGHT,6'
+    elif key_str == 'l':
+        return 'LIGHT,7'
+    elif key_str == ';':
+        return 'LIGHT,8'
+    else:
+        return 'SERVO,1'
 
 # Collect events until released
 with Listener(
