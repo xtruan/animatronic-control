2dcf191e1b93de8ac1daf3d1de69317960cf781c
xtruan <github@xtruan.com>
2020-06-17 18:46:40 -0600

    updating control to work with midi files

--- raspi/control.py
+++ raspi/control.py
@@ -3,8 +3,11 @@ import csv
 import argparse
 import time
 
+from playsound import playsound
+import mido
+
 import utils.transform as transform
-import utils.i2c as i2c
+#import utils.i2c as i2c
 from objects.anim_action import AnimAction
 
 COL_TIME_STR = 0
@@ -13,7 +16,11 @@ COL_DEVICE_TYPE = 2
 COL_DEVICE_ID = 3
 COL_SETTING = 4
 
+
+
 anim_actions = deque([])
+action_viz = {}
+mid_notes = {}
 
 def read_anim_csv(filename, delimiter=',', quotechar='"'):
     csv_file = open(filename, 'r')
@@ -32,18 +39,72 @@ def read_anim_csv(filename, delimiter=',', quotechar='"'):
 
     csv_file.close()
 
-def handle_anim():
+def build_anim_action_from_mid_msg(time_sec, msg):
+    
+    # try:
+    #     if msg.velocity == 0:
+    #         notes[msg.note] = '   '
+    #     else:
+    #         notes[msg.note] = 'XXX'
+    #         if msg.note not in note_count:
+    #             note_count[msg.note] = -1
+    #         note_count[msg.note] += 1
+    #     print(str(notes))
+    #     print(str(note_count))
+    # except AttributeError:
+    #     pass
+
+    try:
+        time_str = str(transform.time_stringify(time_sec))
+        device_i2c_addr = 8
+        device_type = 'LIGHT'
+        device_id = mid_note_to_device_id(msg.note)
+        setting = msg.velocity
+
+        return AnimAction(time_str, time_sec, device_i2c_addr, device_type, device_id, setting)
+    except AttributeError:
+        pass
+    
+    
+def mid_note_to_device_id(note):
+    if note not in mid_notes:
+        mid_notes[note] = len(mid_notes)
+    return mid_notes[note]
+
+def read_anim_mid(filename):
+    mid = mido.MidiFile(filename)
+    
+    time_sec = 0.0
+    for msg in mid:
+        time_sec += msg.time
+        if not msg.is_meta:
+            action = build_anim_action_from_mid_msg(time_sec, msg)
+            if action is not None:
+                anim_actions.append(action)
+        else:
+            print(msg)
+
+def handle_anim(offset=0.0):
+    print('Playing...')
+    print()
+
     start = time.time()
-    end = time.time()
+    end = time.time() + offset
     while len(anim_actions) > 0:
         if anim_actions[0].time_sec <= end - start:
             handle_action(anim_actions.popleft())
-        time.sleep(0.001)
-        end = time.time()
+        time.sleep(0.01)
+        end = time.time() + offset
 
 def handle_action(action):
-    i2c.write_anim_i2c(action)
-    print(action)
+    #i2c.write_anim_i2c(action)
+    
+    if action.setting == 0:
+        action_viz[action.device_id] = '   '
+    else:
+        action_viz[action.device_id] = 'XXX'
+    
+    print('\r{}'.format(str(action_viz)), end="")
 
 def main():
     parser = argparse.ArgumentParser(description="Animatronic Control Program")
@@ -57,9 +118,43 @@ def main():
         read_anim_csv(input_file)
         print('Loaded!')
         wait = input('Press enter to continue...')
+
+        playsound('let_it_go.wav', False)
+
         handle_anim()
+    if '.mid' in input_file.lower():
+        print('Loading file...')
+        read_anim_mid(input_file)
+        print('Loaded!')
+        wait = input('Press enter to continue...')
+
+        playsound('let_it_go.wav', False)
+
+        handle_anim(offset=3.503648)
+
+        # notes = {}
+        # note_count = {}
+        # for msg in mid:
+        #     if not msg.is_meta:
+        #         time.sleep(msg.time)
+        #         #print(str(msg))
+        #         try:
+        #             if msg.velocity == 0:
+        #                 notes[msg.note] = '   '
+        #             else:
+        #                 notes[msg.note] = 'XXX'
+        #                 if msg.note not in note_count:
+        #                     note_count[msg.note] = -1
+        #                 note_count[msg.note] += 1
+        #             print(str(notes))
+        #             print(str(note_count))
+        #         except AttributeError:
+        #             pass
+        
     else:
         raise Exception('Invalid file type')
+
+    time.sleep(5)
 
 if __name__ == '__main__':
     main()
